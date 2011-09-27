5e95fcb7403cb4455912fad268e99f8b3a9aac8f
sgirones <salvador.girones@abiquo.com>
2011-09-27 17:12:52 +0200

    Fixed rules bug

--- main.py
+++ main.py
@@ -25,9 +25,13 @@ if __name__ == '__main__':
             events = get_new_events(last_event,limit=100,dbip=myip,dbuser=myuser,dbpwd=mypwd)
         except Exception, e:
             print("An error ocurred when retrieving events from %s: %s" %(myip, str(e)))
+            
+        if not events:
+            sleep(5)
+            continue
 
-        # Look all users that have any rule
-        for user in load_users_from_rules():
+        # Look for all users
+        for user in load_users(dbip=myip,dbuser=myuser,dbpwd=mypwd):
 
             filtered_events = []
             
@@ -35,37 +39,27 @@ if __name__ == '__main__':
             try:
                 # Load user's rules
                 rules = load_rules_from_user(user)
+                # Include user 'all' rules
                 rules.extend(load_rules_from_user('all'))
             except Exception, e:
                 print("An error ocurred when loading rules: %s" %(str(e)))
 
             
             for r in rules:
-                # Load actions
+                # Load filters
                 actions = r.get_actions()
-                if 'all' in actions:
-                    # Do not filter by Action
-                    action = []
-                
-                # Load owners
                 owners = r.get_owners()
-                if 'all' in owners:
-                    owners = []
-                #If owners is not set, only filter events whose owner is the user.
-                elif not owners:
-                    owners = [r.get_user()]
-                    
-                # Load severity levels
+                if r.get_user() == 'all':
+                    owners = [user]
                 sev_levels = r.get_levels()
-                if 'all' in sev_levels:
-                    # Do not filter by Severity
-                    sev_levels = []
                 
                 # Filter events and add to the list
                 filtered_events.extend(events_to_notify(events, actions, owners, sev_levels))
             
             if filtered_events:
                 print("New events to notify to user: %s" % (user))
+                print("Events to notify: %s"%(filtered_events))
+
                 try:
                     notify_events(user, filtered_events, dbip=myip, dbuser=myuser, dbpwd=mypwd)
                 except Exception, e:

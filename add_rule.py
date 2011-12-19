190952cc2c9230b54baf29940d75f80338df692d
sgirones <salvador.girones@abiquo.com>
2011-12-19 16:14:04 +0100

    Moved list of actions and severity levels to the wiki

--- add_rule.py
+++ add_rule.py
@@ -9,17 +9,7 @@ def usage():
     print "-a\t--actions=ACTIONS\tComma separated list of actions to monitor"
     print "-o\t--owners=OWNERS\t\tComma separated list of owners to filter by. Use 'all' for monitor any owner. If no owner is specified, only monitors actions performed by user"
     print "-s\t--severity=LEVELS\t\tComma separated list of levels of severity to filter by. Use 'all' for monitor any level."
-    print "--list-actions\t\tList all allowed actions"
-    print "--list-severity-levels\t\tList all severity levels"
     print ""
-
-def list_actions():
-    print "VAPP_CREATE - When a VAPP is created"
-    print "VAPP_DELETE - When a VAPP is deleted"
-    #TODO
-    
-def list_severity_levels():
-    print("INFO\nWARNING\nMINOR\nNORMAL\nMAJOR\nCRITICAL")
 
 def main():
     
@@ -29,19 +19,13 @@ def main():
     sev_levels=[]
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "u:a:o:s:LS", ["user=", "actions=", "owners=", "severity=", "list-actions", "list-severity-levels"])
+        opts, args = getopt.getopt(sys.argv[1:], "u:a:o:s:", ["user=", "actions=", "owners=", "severity="])
     except getopt.GetoptError, err:
         print str(err)
         sys.exit(2)
 
     for o, a in opts:
-        if o in ("-L", "--list-actions"):
-            list_actions()
-            sys.exit()
-        elif o in ("-S", "--list-severity-levels"):
-            list_severity_levels()
-            sys.exit()
-        elif o in ("-u", "--user"):
+        if o in ("-u", "--user"):
             user = a.strip()
         elif o in ("-a", "--actions"):
             for action in a.split(','):
@@ -57,15 +41,6 @@ def main():
         usage()
         return
 
-    """
-    print "Add the rule which will filter events"
-    print "If a rule already exists for the target user, it will be replaced"
-
-    user = raw_input("Target user (must be present in Abiquo): ")
-    actions = raw_input("Actions: ")
-    owners = raw_input("Owners: ")
-    """
-    
     rule = Rule(user.strip())
 
     if actions:

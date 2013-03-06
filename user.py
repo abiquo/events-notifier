767674d4e68729a97dc356670ffe07fb189ebe21
daniel <daniel@danfaizer.(none)>
2013-03-06 14:32:27 +0100

    Added license disclaimer

--- user.py
+++ user.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+#       The Abiquo Platform
+#       Cloud management application for hybrid clouds
+#       Copyright (C) 2008-2013 - Abiquo Holding S.L. 
+#
+#       This application is free software; you can redistribute it and/or
+#       modify it under the terms of the GNU LESSER GENERAL PUBLIC
+#       LICENSE as published by the Free Software Foundation under
+#       version 3 of the License
+#
+#       This software is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#       LESSER GENERAL PUBLIC LICENSE v.3 for more details.
+#
+#       You should have received a copy of the GNU Lesser General Public
+#       License along with this library; if not, write to the
+#       Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+#       Boston, MA 02111-1307, USA.
+
 import pycurl
 import StringIO
 from xml.dom.minidom import parse, parseString
